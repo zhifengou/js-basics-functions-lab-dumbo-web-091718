921111b174d16272eeb70df370871a5b39d86e71
zhifeng ou <zhifengou@hotmail.com>
2018-10-27 21:26:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -29,3 +29,15 @@ function distanceTravelledInFeet(loc1,loc2){
   return distance;
 }
 
+function calculatesFarePrice(start, destination) {
+  distance = distanceTravelledInFeet(start, destination)
+  if (distance < 400) {
+    return 0;
+  } else if (distance > 400 && distance < 2000) {
+    return distance * .02;
+  } else if (distance > 2000 && distance < 2500) { 
+    return 25;
+  } else {
+    return 'cannot travel that far';
+  }
+}
