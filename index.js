83b5cfe68af99777728e0e132515c51c0120da85
zhifeng ou <zhifengou@hotmail.com>
2018-10-27 21:38:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ function calculatesFarePrice(loc1, loc2) {
   if (distance < 400) {
     return 0;
   } else if (distance > 400 && distance < 2000) {
-    return distance * .02;
+    return distance * 0.02;
   } else if (distance > 2000 && distance < 2500) { 
     return 25;
   } else {
