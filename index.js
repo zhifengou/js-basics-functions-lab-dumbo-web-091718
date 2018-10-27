1cc6c008a9bfd9351772168304787ebda610e736
zhifeng ou <zhifengou@hotmail.com>
2018-10-27 21:39:13 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // Code your solution in this file!
+// function happyHolidays(){
+//   return "Happy holidays!"
+// }
+// function happyHolidaysTo(name){
+//   return 'Happy holidays,${name}!'
+// }
+
+// function happyHolidayTo(holiday,name){
+//   return 'Happy ${holiday}.${name}!'
+// }
+
+// function holidayCountdown(holiday,days){
+//   return 'It\'s ${days} until ${holiday}'
+// }
+function distanceFromHqInBlocks(location) {
+  let distance;
+  location>=42?distance=location-42:distance=42-location;
+  return distance;
+} 
+function distanceFromHqInFeet(location){
+  let distance;
+  location>=42?distance=(location-42)*264:distance=(42-location)*264;
+  return distance;
+}
+function distanceTravelledInFeet(loc1,loc2){
+  let distance;
+  loc2>=loc1?distance=(loc2-loc1)*264:distance=(loc1-loc2)*264;
+  return distance;
+}
+
+function calculatesFarePrice(loc1, loc2) {
+  distance = distanceTravelledInFeet(loc1, loc2)
+  if (distance < 400) {
+    return 0;
+  } else if (distance > 400 && distance < 2000) {
+    distance -=400;
+    return distance * 0.02;
+  } else if (distance > 2000 && distance < 2500) { 
+    return 25;
+  } else {
+    return 'cannot travel that far';
+  }
+}
